55f679448ba14aa74a637327559644f260619cee
hayRenat <73642042+hayRenat@users.noreply.github.com>
2021-03-08 19:37:36 +0300

    Логгирование в чате

--- server/src/main/java/ru/geekbrains/server/ClientHandler.java
+++ server/src/main/java/ru/geekbrains/server/ClientHandler.java
@@ -1,5 +1,8 @@
 package ru.geekbrains.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -17,6 +20,7 @@ public class ClientHandler {
     private String nickname;
     //добавил формат даты для логов
     SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss dd.MM.yyyy");
+    private static Logger logger = LogManager.getLogger();
 
     public ClientHandler(Server server, Socket socket) {
         try {
@@ -35,20 +39,25 @@ public class ClientHandler {
                         if (str.startsWith("/auth")) {
                             String[] subStrings = str.split(" ", 3);
                             if (subStrings.length == 3) {
+                                logger.info("Пользователь пытается авторизоваться с логином " + subStrings[1]);
                                 String nickFromDB = SQLHandler.getNickByLoginAndPassword(subStrings[1], subStrings[2]);
                                 if (nickFromDB != null) {
                                     if (!server.isNickInChat(nickFromDB)) {
                                         nickname = nickFromDB;
                                         sendMsg("/authok " + nickname);
                                         server.subscribe(ClientHandler.this);
+                                        logger.info("Упешная авторизация пользователя " + subStrings[1]);
                                         break;
                                     } else {
+                                        logger.warn("Пользователь " + subStrings[1] + " уже ранее авторизован в чате");
                                         sendMsg("This nick already in use");
                                     }
                                 } else {
+                                    logger.warn("Неудачная попытка авторизации " + subStrings[1]);
                                     sendMsg("Wrong login/password");
                                 }
                             } else {
+                                logger.warn("Пользователь ввёл не корректные данные для входа");
                                 sendMsg("Wrong data format");
                             }
                         }
@@ -56,9 +65,12 @@ public class ClientHandler {
                             String[] subStr = str.split(" ");
                             // /registration login pass nick
                             if (subStr.length == 4) {
+                                logger.info("Попытка регистрации. Пользователь ввёл login = " + subStr[1] + " password + subStr[3]");
                                 if (SQLHandler.tryToRegister(subStr[1], subStr[2], subStr[3])) {
+                                    logger.info("Успешная регистрация пользователя " + subStr[1]);
                                     sendMsg("Registration complete");
                                 } else {
+                                    logger.warn("Неудачная попытка регистрации пользователя " + subStr[1] + ", с ником " + subStr[3]);
                                     sendMsg("Incorrect login/password/nickname");
                                 }
                             }
@@ -71,6 +83,7 @@ public class ClientHandler {
                         System.out.println("[" + dateFormat.format(new Date()) + "]" + "Сообщение от клиента: " + str);
                         if (str.startsWith("/")) {
                             if (str.equals("/end")) {
+                                logger.info("Пользователь " + nickname + " выходит из чата");
                                 break;
                             } else if (str.startsWith("/w")) {
                                 // личные сообщения
@@ -82,9 +95,11 @@ public class ClientHandler {
                                         server.unicastMsg(toUserNick, "[" + dateFormat.format(new Date()) + "]" + " " + "from " + nickname + ": " + subStrings[2]);
                                         sendMsg("[" + dateFormat.format(new Date()) + "]" + " " + "to " + toUserNick + ": " + subStrings[2]);
                                     } else {
+                                        logger.warn("Неудачная попытка отправки личного сообщения от пользователя " + nickname + ", т.к. пользователя с таким ником " + toUserNick + " нет в чате");
                                         sendMsg("User with nick '" + toUserNick + "' not found in chat room");
                                     }
                                 } else {
+                                    logger.warn("Недостаточно данных для отправки личного сообщения от пользователя" + nickname);
                                     sendMsg("Wrong private message");
                                 }
                             } else if (str.startsWith("/changenick")){
@@ -94,9 +109,11 @@ public class ClientHandler {
                                     if (SQLHandler.tryToChangeNick(subStr[1], nickname)){
                                         sendMsg("[" + dateFormat.format(new Date()) + "]" + nickname + " изменён на " + subStr[1]);
                                         sendMsg("Изменения вступят в силу после перезахода");
+                                        logger.warn("Пользователь " + nickname + "изменил ник на " + subStr[1] + " нет в чате");
                                         server.broadcastMsg("[" + dateFormat.format(new Date()) + "]" + nickname + " changed has nickname to " + subStr[1]);
                                         break;
                                     } else {
+                                        logger.warn("Неудачная попытка изменения ника ользователя " + nickname + "на " + subStr[1]);
                                         sendMsg("Incorrect nickname");
                                     }
                                 }
@@ -109,18 +126,24 @@ public class ClientHandler {
                     e.printStackTrace();
                 } finally {
                     try {
+                        logger.debug("Попытка закрытия входящего потока");
                         in.close();
                     } catch (IOException e) {
+                        logger.error("Закрытие входящего потока завершилось неудачно" , e);
                         e.printStackTrace();
                     }
                     try {
+                        logger.debug("Попытка закрытия исхдящего потока");
                         out.close();
                     } catch (IOException e) {
+                        logger.error("Закрытие исходящего потока завершилось неудачно" , e);
                         e.printStackTrace();
                     }
                     try {
+                        logger.debug("Попытка закрытия сокета");
                         socket.close();
                     } catch (IOException e) {
+                        logger.error("Закрытие сокета завершилось неудачно" , e);
                         e.printStackTrace();
                     }
                     server.unsubscribe(ClientHandler.this);
@@ -129,6 +152,7 @@ public class ClientHandler {
             });
             executorService.shutdown();
         } catch (IOException e) {
+            logger.error("Ошибка исполнения ClientHandler" , e);
             e.printStackTrace();
         }
     }
@@ -137,6 +161,7 @@ public class ClientHandler {
         try {
             out.writeUTF(msg);
         } catch (IOException e) {
+            logger.error("Неудачная отправка сообщения" , e);
             e.printStackTrace();
         }
     }
--- server/src/main/java/ru/geekbrains/server/SQLHandler.java
+++ server/src/main/java/ru/geekbrains/server/SQLHandler.java
@@ -1,26 +1,34 @@
 package ru.geekbrains.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.sql.*;
 
 
 public class SQLHandler {
     private static Connection connection;
     private static Statement statement;
+    private static Logger logger = LogManager.getLogger();
 
     public static void connect() {
         try {
             Class.forName("org.sqlite.JDBC");
             connection = DriverManager.getConnection("jdbc:sqlite:server/database.db");
             statement = connection.createStatement();
+            logger.info("Успешное подключение к БД");
         } catch (Exception e) {
+            logger.error("Не удалось подключиться к базе данных " + connection.toString());
             e.printStackTrace();
         }
     }
 
     public static void disconnect() {
         try {
+            logger.info("Отключение от БД");
             connection.close();
         } catch (SQLException e) {
+            logger.error("Не удалось отключитсья от БД");
             e.printStackTrace();
         }
     }
@@ -28,32 +36,44 @@ public class SQLHandler {
     public static String getNickByLoginAndPassword(String login, String password) {
         try {
             ResultSet rs = statement.executeQuery("SELECT nickname FROM users WHERE login ='" + login + "' AND password = '" + password + "'");
+            logger.debug("Проверка комбинации login = " + login + " password = " + password);
             if (rs.next()) {
+                logger.debug("Пользователь найден");
                 return rs.getString("nickname");
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        logger.debug("Пользователь не найден");
         return null;
     }
 
     public static boolean tryToRegister(String login, String password, String nickname) {
         try {
+            logger.info("Попытка регистрации пользователя с ником " + nickname + " и с логином " + login);
             statement.executeUpdate("INSERT INTO users (login, password, nickname) VALUES ('" + login + "','" + password + "','" + nickname + "')");
+            logger.info("Пользователь " + login + " успешно зарегестрирован");
             return true;
         } catch (SQLException e) {
+            logger.warn("Неудачная регистрация пользователя login = " + login, " nickname = " + nickname);
             return false;
         }
     }
 
     public static boolean tryToChangeNick(String newNick, String oldNick) {
-        try { ResultSet rs = statement.executeQuery("SELECT nickname FROM users WHERE nickname = '" + oldNick + "'");
+        try {
+            logger.info("Попытка пользователя с ником " + oldNick + " изменить ник на " + newNick);
+            ResultSet rs = statement.executeQuery("SELECT nickname FROM users WHERE nickname = '" + oldNick + "'");
                 if (rs.next()) {
+                    logger.info("Пользователь " + oldNick + " изменил ник на " + newNick);
             statement.executeUpdate("UPDATE users SET nickname = '" + newNick + "' WHERE nickname = '" + oldNick + "'");
-            return true;}
+            return true;
+                }
         } catch (SQLException e) {
+            logger.error("Неудачная попытка пользователя с ником" + oldNick + " изменить ник " + newNick);
             return false;
         }
+        logger.warn("Неудачная попытка пользователя с ником" + oldNick + " изменить ник " + newNick);
         return false;
     }
 }
